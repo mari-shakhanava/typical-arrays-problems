56067c59ea454775c39d370cdad43da2526ff405
mari-shakhanava <maria.shakhanava@gmail.com>
2020-09-29 14:11:53 +0300

    feat: write algoritms of mix max avg items in aaray

--- src/index.js
+++ src/index.js
@@ -1,12 +1,39 @@
 
 exports.min = function min (array) {
-  return 0;
-}
+    let minItem = 0;
+    let newArray =[];
+    if (!array || array.length === 0){
+        return 0;
+        } else
+        newArray = array.sort( (a, b) => a - b );
+        minItem = newArray[0];
+        return minItem;
+    }
+
 
 exports.max = function max (array) {
-  return 0;
-}
+    let maxItem = 0;
+    let newArray =[];
+    if (!array || array.length === 0){
+        return 0;
+    } else
+        newArray = array.sort( (a, b) => a - b );
+        maxItem = newArray[newArray.length-1];
+        return maxItem;
+    }
+
 
 exports.avg = function avg (array) {
-  return 0;
+    let sum = 0;
+    let n = 0;
+    let avg = 0;
+    if (!array || array.length === 0){
+        return 0;
+    } else
+      for (i=0; i<array.length; i++){
+            sum += array[i];
+            n++;}
+        return avg = sum/n;
 }
+
+
